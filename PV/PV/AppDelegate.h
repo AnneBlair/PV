a899d1d42db31b295b189d6ca81475137173422b
语歌 <291181204@qq.com>
2016-07-04 01:14:10 +0800

    1.0

---
+++ PV/PV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PV
+//
+//  Created by Sir-Anne-Blair on 16/7/4.
+//  Copyright © 2016年 anne-blair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PV/PV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PV
+//
+//  Created by Sir-Anne-Blair on 16/7/4.
+//  Copyright © 2016年 anne-blair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
